b4ef49928bf7461177444c502febe73f1f9cefee
jdelgadoptg <j.delgado@pitagorinesgroup.com>
2025-11-19 08:03:56 +0100

    feat: añadir campos de rectificación a la tabla billing_hashes

---
+++ app/Database/Migrations/2025-11-17-161359_AddRectifyFieldsToBillingHashes.php
@@ -0,0 +1,40 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+final class AddRectifyFieldsToBillingHashes extends Migration
+{
+    public function up()
+    {
+        $this->forge->addColumn('billing_hashes', [
+            'invoice_type' => [
+                'type'       => 'VARCHAR',
+                'constraint' => 4,
+                'null'       => true,
+                'after'      => 'issue_date',
+            ],
+            'rectified_billing_hash_id' => [
+                'type'       => 'INT',
+                'unsigned'   => true,
+                'null'       => true,
+                'after'      => 'invoice_type',
+            ],
+            'rectified_meta_json' => [
+                'type' => 'TEXT',
+                'null' => true,
+                'after' => 'rectified_billing_hash_id',
+            ],
+        ]);
+    }
+
+    public function down()
+    {
+        $this->forge->dropColumn('billing_hashes', 'invoice_type');
+        $this->forge->dropColumn('billing_hashes', 'rectified_billing_hash_id');
+        $this->forge->dropColumn('billing_hashes', 'rectified_meta_json');
+    }
+}
