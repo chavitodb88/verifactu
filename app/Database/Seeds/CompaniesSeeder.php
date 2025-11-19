2deac1c76eeac74ac21be1e56024f0ca048e6e1c
jdelgadoptg <j.delgado@pitagorinesgroup.com>
2025-11-19 14:13:55 +0100

    feat: incluir el campo issuer_nif en el seeder de la tabla companies

--- app/Database/Seeds/CompaniesSeeder.php
+++ app/Database/Seeds/CompaniesSeeder.php
@@ -11,15 +11,16 @@ final class CompaniesSeeder extends Seeder
     public function run()
     {
         $this->db->table('companies')->insert([
-            'slug'               => 'acme',
-            'name'               => 'ACME S.L.',
-            'verifactu_enabled'  => 1,
-            'send_to_aeat'       => 0,
-            'nif_colaborador'    => null,
-            'storage_driver'     => 'fs',
-            'storage_base_path'  => 'acme/',
-            'created_at'         => date('Y-m-d H:i:s'),
-            'updated_at'         => date('Y-m-d H:i:s'),
+            'slug'                       => 'acme',
+            'name'                       => 'ACME S.L.',
+            'issuer_nif'                 => 'B61206934',
+            'verifactu_enabled'          => 1,
+            'send_to_aeat'               => 0,
+            'nif_colaborador'            => null,
+            'storage_driver'             => 'fs',
+            'storage_base_path'          => 'acme/',
+            'created_at'                 => date('Y-m-d H:i:s'),
+            'updated_at'                 => date('Y-m-d H:i:s'),
         ]);
     }
 }
