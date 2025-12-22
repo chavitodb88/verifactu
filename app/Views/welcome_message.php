12f46dc57761bb332ca9ba9a21277db532606dbc
jdelgadoptg <j.delgado@pitagorinesgroup.com>
2025-12-22 16:32:12 +0100

    feat: corregir título de la página de bienvenida eliminando el punto final

--- app/Views/welcome_message.php
+++ app/Views/welcome_message.php
@@ -4,11 +4,11 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>API VERI*FACTU.</title>
+    <title>API VERI*FACTU</title>
 </head>
 
 <body>
-    <h1>Welcome to API VERI*FACTU</h1>
+    <h1>Welcome to API VERI*FACTU.</h1>
     <p>This is the welcome message for the VERI*FACTU API.</p>
 
 </body>
